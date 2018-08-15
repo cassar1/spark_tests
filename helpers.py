7f22b87ca31a3e57ed70e1909bfa8e729ecaf966
jurgen <jurgen.cassar@hotmail.com>
2018-08-15 11:09:17 +0200

    DistributedLSH Implementation

--- helpers.py
+++ helpers.py
@@ -1,3 +1,8 @@
+from __future__ import division
+
+import sys
+import numpy as np
+
 from rdkit import DataStructs
 from rdkit import Chem
 from rdkit.Chem import AllChem
@@ -143,3 +148,147 @@ def persistDataToFile(data):
     for item in data:
         file.write("%s\n" % item)
     file.close()
+
+########################################################################
+#                               LSH
+########################################################################
+
+
+
+
+def get_permutations(len_permutations=1024, num_permutations=100):
+    return map(lambda _: np.random.permutation(len_permutations), range(num_permutations))
+
+
+def get_buckets_allowed(min_hash):
+    num_bits = (1024 - 1).bit_length()
+    max_hashes_bucket = int(sys.maxint.bit_length() / num_bits)
+    min_buckets = int(min_hash / max_hashes_bucket)
+
+    bucket_sizes_allowed = []
+    for i in range(min_buckets, 1000):
+        if i is not 0 and not (min_hash % i):
+            print "allowed buckets ", i
+            hash_per_bucket = int(min_hash / i)
+
+            if num_bits * hash_per_bucket > sys.maxint.bit_length():
+                print 'numbers are too large to produce valid buckets'
+
+            print "Hashes per bucket:", hash_per_bucket
+            bucket_sizes_allowed.append(i)
+    return bucket_sizes_allowed
+
+
+def get_min_hash(fingerprint, permutations):
+    # qfp_bits = [int(n) for n in AllChem.GetMorganFingerprintAsBitVect(mol, 2, nBits=2048)]
+    qfp_bits = [int(n) for n in fingerprint]
+    min_hash = []
+    for perm in permutations:
+        for idx, i in enumerate(perm):
+            if qfp_bits[i]:
+                min_hash.append(idx)
+                break
+    return min_hash
+
+
+def hash_to_buckets(min_hash, num_buckets=10, nBits=1024):
+
+    if len(min_hash) % num_buckets:
+        raise Exception('number of buckets must be divisiable by the hash length')
+    buckets = []
+    hash_per_bucket = int(len(min_hash) / num_buckets)
+
+    num_bits = (nBits - 1).bit_length()
+
+    if num_bits * hash_per_bucket > sys.maxint.bit_length():
+        raise Exception('numbers are too large to produce valid buckets')
+    for b in range(num_buckets):
+
+        buckets.append(reduce(lambda x, y: (x << num_bits) + y, min_hash[b:(b + hash_per_bucket)]))
+    return buckets
+
+
+def get_threshold_neighbours(bc_fingerprints, target_fp_idx, ids_to_compare, threshold):
+    valid_neighbours = []
+
+    for comparing_fp in ids_to_compare:
+        if comparing_fp != target_fp_idx:
+            target_fp = bc_fingerprints[target_fp_idx]
+            nbr_fp = bc_fingerprints[comparing_fp]
+            similarity = DataStructs.FingerprintSimilarity(target_fp, nbr_fp, metric=DataStructs.TanimotoSimilarity)
+            if similarity > threshold:
+                valid_neighbours.append(comparing_fp)
+    return valid_neighbours
+
+
+def hash_fingerprints(fingerprint, fp_index, num_buckets, permutations):
+    hash_list = []
+    # a dictionary with id of fingerprint and the list of hashes that it makes part of
+    index_hashes = {}
+
+    min_hash = get_min_hash(fingerprint, permutations)
+    buckets = hash_to_buckets(min_hash, num_buckets)
+    index_hashes[fp_index] = set()#[]
+    for hash_bucket in buckets:
+        #if hash_bucket not in hash_list:
+            #hash_list[hash_bucket] = set()#[]
+
+        hash_list.append((hash_bucket,set([fp_index])))
+        index_hashes[fp_index].add(hash_bucket)
+
+    return [index_hashes, hash_list]
+
+
+def get_neighbours(index_hashes, hash_list, target_fp_idx):
+    potential_neighbours = set()
+
+    fp_hashes = index_hashes[target_fp_idx]
+
+    for hash_id in fp_hashes:
+        #for key in index_hashes:
+        #    if hash_id in index_hashes[key]:
+        #        potential_neighbours.add(key)
+        potential_neighbours_in_hash = hash_list[hash_id]
+        potential_neighbours.update(potential_neighbours_in_hash)
+
+    #sorted_neighbours = sorted(potential_neighbours)
+    return potential_neighbours
+
+def get_threshold_neighbours_flat(bc_fingerprints, target_fp_idx, ids_to_compare, threshold):
+    valid_neighbours = []
+
+    for comparing_fp in ids_to_compare:
+        if comparing_fp != target_fp_idx:
+            target_fp = bc_fingerprints[target_fp_idx]
+            nbr_fp = bc_fingerprints[comparing_fp]
+            similarity = DataStructs.FingerprintSimilarity(target_fp, nbr_fp, metric=DataStructs.TanimotoSimilarity)
+            if similarity > threshold:
+                # print "fp:", comparing_fp, " sim: ", similarity
+                valid_neighbours.append((target_fp_idx, comparing_fp))
+
+    return valid_neighbours
+
+def get_threshold_neighbours_flat2(bc_fingerprints, target_fp_idx, ids_to_compare, threshold):
+    valid_neighbours = []
+    nbr_fps = []
+    for comparing_fp in ids_to_compare:
+        nbr_fps.append(bc_fingerprints[comparing_fp])
+    target_fp = bc_fingerprints[target_fp_idx]
+
+    similarity_list = DataStructs.BulkTanimotoSimilarity(target_fp, nbr_fps)
+
+    for idx, val in enumerate(ids_to_compare):
+        if similarity_list[idx] > threshold:
+            # print "fp:", comparing_fp, " sim: ", similarity
+            valid_neighbours.append((target_fp_idx, val))
+
+    return valid_neighbours
+
+
+def get_neighbours_flat(target_fp_idx, ids_to_compare):
+    valid_neighbours = []
+
+    for comparing_fp in ids_to_compare:
+        valid_neighbours.append((target_fp_idx, comparing_fp))
+
+    return valid_neighbours
--- moltests.py
+++ moltests.py
@@ -1,26 +1,19 @@
 from __future__ import print_function
 from pyspark import SparkConf, SparkContext
-from pyspark.mllib.linalg.distributed import RowMatrix
 from pyspark.mllib.linalg import Vectors
 from pyspark.sql import SparkSession, Row
-from pyspark.sql import SQLContext
+from pyspark.ml.feature import MinHashLSH
+from pyspark.ml.linalg import Vectors
+import pyspark.sql.functions as f
 import sys
 import time
+import datetime
 
-#conf = SparkConf().setAppName("DistributedClustering")
-conf = SparkConf() \
-    .setMaster("local").setAppName("MoleculesTests")
-#conf = SparkConf().setMaster(server_location).setAppName("MoleculesTests")
-sc = SparkContext(conf=conf)
-
-sc.addPyFile("helpers.py")
-
-from helpers import *
-
-EVALUATION = True
+EVALUATION = False
 FLATMAPMETHOD = True
 CREATE_VECTORS = False
-SIMILARITY_THRESHOLD = 0.3
+SIMILARITY_THRESHOLD = 0.7
+LSH_SIMILARITY = True
 #dataFile = '../mols/compounds18.smi'
 baseFile = '../mols/'
 #baseFile = 'wasb://molecules-container@jurgenhdinsightstorage.blob.core.windows.net/'
@@ -28,24 +21,45 @@ baseFile = '../mols/'
 executor_num = 1
 #dataFile = '../mols/merged/Reninmerged.smi'
 
+#conf = SparkConf().setAppName("DistributedClustering")
+conf = SparkConf() \
+    .setMaster("local").setAppName("MoleculesTests")
+#conf = SparkConf().setMaster(server_location).setAppName("MoleculesTests")
+sc = SparkContext(conf=conf)
+spark = SparkSession.builder.config(conf = conf).getOrCreate()
+sc.addPyFile("helpers.py")
+
+from helpers import *
+
 def main():
+    print ("START ",datetime.datetime.now())
     compounds = load_data(sc, dataFile)
     #example entry in compounds: ((u'C[N+](C)(C)[C@@H]1[C@@H](O)O[C@H]2[C@@H](O)CO[C@H]21', <rdkit.Chem.rdchem.Mol object at 0x7f7dc0cd9050>, <rdkit.DataStructs.cDataStructs.ExplicitBitVect object at 0x7f7dc0cd90c0>, u'ZINC000000039940'), 0)
-
+    compounds.partitionBy(executor_num)
     fingerprints = select_fingerprints(compounds)
 
-    fingerprints1 = fingerprints.partitionBy(executor_num).cache()
+    fingerprints1 = fingerprints
+    #fingerprints1 = fingerprints.partitionBy(executor_num).cache()
 
-    fingerprints1.foreach(output)
+    #fingerprints1.foreach(output)
     bc_fingerprints = sc.broadcast(fingerprints1.collectAsMap())
+    if not LSH_SIMILARITY:
+        neighbour_similarities = calculate_neighbours_similarities(fingerprints1, bc_fingerprints).cache()
+    else:
+        #vector_fps = fingerprints_to_vectors(fingerprints1)
+        #bc_fingerprints = sc.broadcast(vector_fps.collectAsMap())
+        #neighbour_similarities = calculate_LSH_neighbours(fingerprints1).cache()
+        neighbour_similarities = calculate_LSH_custom(bc_fingerprints, fingerprints).cache()
 
-    neighbour_similarities = calculate_neighbours_similarities(fingerprints1, bc_fingerprints).cache()
-
+    #neighbour_similarities.foreach(output)
+    #print (neighbour_similarities.count())
     # for each molecule set (mol_id,1) to then reduce and finally get the final number
     molecules_appearances = neighbour_similarities.map(lambda (mol_1,mol_2): (mol_1,1))
+
     mol_neighbour_count = molecules_appearances.reduceByKey(lambda a, b: a + b)
 
     bc_mol_neighbour_count = sc.broadcast(mol_neighbour_count.collectAsMap())
+    #counts = neighbour_similarities.countByKey()
 
     # change from (mol_id, nbr_id) to (mol_id, (nbr_id, nbr_count), nbr_count)
     complete_similarities_fp = neighbour_similarities.map(lambda (a, b): (a, convert_single_neighbour_dict(b, bc_mol_neighbour_count.value),convert_owner_dict(a, bc_mol_neighbour_count.value)))
@@ -59,9 +73,9 @@ def main():
     neighbours = subset_similarities_fp.reduceByKey(lambda a, b: a.union(b))
 
     neighbours = neighbours.map(lambda (mol, nbrs): (mol, sorted(nbrs, key=lambda x: (-x[1], x[0])))).cache()
-    neighbours.foreach(output)
+    #neighbours.foreach(output)
     # return (mol_id, neighbours, nbr_count of mol_id, -1)
-    cluster_assignment = neighbours.map(lambda (a, b): (a,b,convert_owner_dict(a,bc_mol_neighbour_count.value),-1))
+    cluster_assignment = neighbours.map(lambda (a, b): (a,b,convert_owner_dict(a, bc_mol_neighbour_count.value),-1))
 
     # assign cluster
     bc_invalid_clusters = sc.broadcast([])
@@ -106,8 +120,6 @@ def main():
         cluster_assignment = cluster_assignment.filter(lambda (mol_id, nbr_counts, mol_count, cluster_assigned): mol_id not in bc_valids.value).cache()
         iteration += 1
 
-
-
     bc_valid_clusters = sc.broadcast(total_valids.collectAsMap())
 
     cluster_centers_single_neighbours = complete_similarities_fp.filter(lambda(mol_id, nbr, nbr_counts):mol_id in bc_valid_clusters.value)\
@@ -121,6 +133,9 @@ def main():
     cluster_combs = cluster_combs.map(lambda (a,b): (a[0], set(a[1]).difference(set(b[1]))) if a[0] != b[0] else (a[0], set(a[1])))
 
     cluster_combs = cluster_combs.reduceByKey(lambda a,b: a.intersection(b))
+    print("End ", datetime.datetime.now())
+    cluster_combs = cluster_combs.sortByKey()
+    #cluster_combs.foreach(output)
 
     #if EVALUATION:
     #    cluster_combs = cluster_combs.sortBy(lambda (a,b): a)
@@ -295,6 +310,84 @@ def calculate_neighbours_similarities(fingerprints, bc_fingerprints):
 
     return complete_similarities_fp
 
+def fingerprints_to_vectors(fingerprints):
+    print("fingerprints_to_Vectors")
+    vector_fingerprints = fingerprints.map(lambda (idx, x): (
+        idx, Vectors.sparse(len(x), [(index, value) for (index, value) in enumerate(x) if value != 0]))).cache()
+    return vector_fingerprints
+
+
+def calculate_LSH_neighbours(fingerprints):
+    print("calculate_LSH_neighbours")
+    vector_fingerprints = fingerprints.map(lambda (idx, x): (
+    idx, Vectors.sparse(len(x), [(index, value) for (index, value) in enumerate(x) if value != 0]))).cache()
+
+    # represent fingerprints as a vector
+    schemaFps = spark.createDataFrame(vector_fingerprints, ["id", "features"]).cache()
+
+    vector_fingerprints.unpersist()
+
+    mh = MinHashLSH(inputCol="features", outputCol="hashes", numHashTables=5)
+    model = mh.fit(schemaFps)
+
+
+    mol_similarity = model.approxSimilarityJoin(schemaFps, schemaFps, 1 - SIMILARITY_THRESHOLD,distCol="JaccardDistance")\
+        .select(f.col("datasetA.id").alias("source_id"),
+                f.col("datasetB.id").alias("target_id"),
+                f.col("JaccardDistance"))
+
+    #filtered_similarity = mol_similarity.filter(mol_similarity.JaccardDistance > SIMILARITY_THRESHOLD)
+    mol_similarity.orderBy("source_id").show(100)
+    rdd = mol_similarity.rdd.map(tuple)
+
+
+    neighbour_list = rdd.map(lambda (source,target,similarity): (source,target))
+    return neighbour_list
+
+def calculate_LSH_custom(bc_fingerprints, fingerprints):
+    num_hashes = 100
+
+    permutations = get_permutations(1024, num_hashes)
+    #print("PERMUTATIONS ", permutations)
+    #print (len(permutations[0]))
+    #print(len(permutations))
+    buckets_allowed = get_buckets_allowed(num_hashes)
+
+    bucket = buckets_allowed[0]
+
+    fingerprints_hashes = fingerprints.map(
+        lambda (idx, fp): (idx, fp, hash_fingerprints(fp, idx, bucket, permutations))).cache()
+
+    #index_hashes = fingerprints_hashes.map(lambda (idx, fp, hash): (idx, hash[idx]))
+    #print("Index Hashes ",index_hashes.count())
+    #bc_hash = sc.broadcast(index_hashes.collectAsMap())
+
+    #index_hashes = fingerprints_hashes.map(lambda (idx, fp, hashes): (idx, hashes[0][idx]))
+    fingerprints_hashes.foreach(output)
+    #index_hashes.foreach(output)
+    hash_list = fingerprints_hashes.flatMap(lambda (idx, fp, hashes): hashes[1])
+
+    hash_list.foreach(output)
+    print(hash_list.count())
+    complete_hash_list = hash_list.reduceByKey(lambda a, b: a.union(b))
+    #complete_hash_list.foreach(output)
+
+    #bc_hash = spark.sparkContext.broadcast(index_hashes.collectAsMap())
+    bc_hash_list = spark.sparkContext.broadcast(complete_hash_list.collectAsMap())
+
+    #potential_neighbours = fingerprints_hashes.map(lambda (idx, fp, hash): (idx, get_neighbours(bc_hash.value, idx)))
+    #UPDATE HERE..... INDEX HASHES DOES NOT REQUIRE BROADCAST
+    potential_neighbours = fingerprints_hashes.map(
+        lambda (idx, fp, hash): (idx, get_neighbours(hash[0], bc_hash_list.value, idx)))
+    # neighbours = potential_neighbours.map(lambda (idx, nbrs): (idx, get_threshold_neighbours(bc_fingerprints.value, idx, nbrs, SIMILARITY_THRESHOLD)))
+
+    #potential_neighbours.foreach(output)
+
+    neighbours = potential_neighbours.flatMap(
+        lambda (idx, nbrs): (get_threshold_neighbours_flat2(bc_fingerprints.value, idx, nbrs, SIMILARITY_THRESHOLD)))
+
+    complete_neighbours = neighbours.flatMap(lambda (a, b): [(a, b)] if a == b else [(a, b), (b, a)])
+    return complete_neighbours
 
 def get_neighbours_counts(neighbours):
     neighbours = neighbours.map(lambda (a,b):(a,b,len(b)))
